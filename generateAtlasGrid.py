6b1c9d17d229ed0c1c6a2937ffda22975a45dbd7
zuggamasta <34277191+zuggamasta@users.noreply.github.com>
2019-07-28 21:05:51 +0200

    Create generateAtlasGrid.py, to automate "bake"

---
+++ generateAtlasGrid.py
@@ -0,0 +1,26 @@
+import bpy, math
+
+imageResolution  = 2048
+
+bpy.context.scene.render.resolution_y = imageResolution
+bpy.context.scene.render.resolution_x = imageResolution
+
+size = len(bpy.data.materials)
+
+cameraScale = math.ceil(math.sqrt(size))
+cameraPos = cameraScale/2-0.5
+bpy.ops.object.camera_add(enter_editmode=False, location=(cameraPos, cameraPos, 1), rotation=(0, -0, 0))
+bpy.context.object.data.type = 'ORTHO'
+bpy.context.object.data.ortho_scale = cameraScale
+
+mat = bpy.data.materials
+
+squares = 0
+for i in range(math.ceil(math.sqrt(size))):
+    for j in range(math.ceil(math.sqrt(size))):
+        if(squares < size):
+            bpy.ops.mesh.primitive_plane_add(size=1, enter_editmode=False, location=(i, j, 0))
+            activeObject = bpy.context.active_object #Set active object to variable
+            activeObject.data.materials.append(mat[squares]) #add the material to the object
+            #bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=5)
+        squares += 1;
